14376a68e3de8c2431556fdd52897687747f6201
Kyle Buchanan <kylebuch8@gmail.com>
2015-08-08 12:29:22 -0400

    delete an alarm

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var crypto = require('crypto');
 
 io.on('connection', function (socket) {
     socket.on('createAlarm', function (data) {
         io.emit('alarm', {
+            id: crypto.randomBytes(20).toString('hex'),
             date: data.date
+        });
+    });
+
+    socket.on('deleteAlarm', function (data) {
+        io.emit('deleteAlarm', {
+            id: data.id
         });
     });
 });
